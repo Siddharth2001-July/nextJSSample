898ef96efb3ad33be4f4b58811b7835a3a5682ad
Siddharth Kamboj <71596674+Siddharth2001-July@users.noreply.github.com>
2024-07-18 12:09:36 +0530

    added pspdfkit cdn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
-
+declare global {
+  interface Window { PSPDFKit: any; }
+}
 export default function RootLayout({
   children,
 }: {
@@ -8,7 +10,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <script src="https://cdn.cloud.pspdfkit.com/pspdfkit-web@2024.4.0/pspdfkit.js"></script>
+      </body>
     </html>
   );
 }
